76c6d3943ecc1cf7bb4545670ab07468d93f0dc4
Harun Hasagić <harunhasagic2@gmail.com>
2025-07-22 01:24:50 +0200

    bug fixes

--- backend/repositories/user_repository.py
+++ backend/repositories/user_repository.py
@@ -2,15 +2,12 @@ from sqlmodel import Session, select
 from models import User
 from typing import Optional
 
-class UserRepository:
-    @staticmethod
-    def get_by_email(session: Session, email: str) -> Optional[User]:
-        statement = select(User).where(User.email == email)
-        return session.exec(statement).first()
+def get_user_by_email(session: Session, email: str) -> Optional[User]:
+    statement = select(User).where(User.email == email)
+    return session.exec(statement).first()
 
-    @staticmethod
-    def create_user(session: Session, user: User) -> User:
-        session.add(user)
-        session.commit()
-        session.refresh(user)
-        return user 
+def create_user(session: Session, user: User) -> User:
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user 
